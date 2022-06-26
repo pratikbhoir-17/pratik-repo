8851c4a4ce743bc85dc0172627190f1a9a9a4e6c
PRATIK BHOIR <Prateik@PRATIKs-MacBook-Pro.local>
2022-06-26 21:05:20 +0530

    modified add program

--- add.java
+++ add.java
@@ -2,11 +2,12 @@ class Add
 
     public static void main (String[] args)
     {
+     
     int x,y,c=0;
     x=10;
     y=20;
     addIs(x,y);
-    
+    system.out.println("addition of numbers")
 }
 
 public static int addIs(int a, int b)
