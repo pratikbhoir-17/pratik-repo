a810ee0150d063d09e774e13064afeb18e899b07
pratikbhoir-17 <104353848+pratikbhoir-17@users.noreply.github.com>
2022-06-26 20:57:53 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,15 @@
+class Addition
+{
+    public static void main (String []args)
+    {
+        Addition ad=new Addition();
+        System.out.println("sum is :"+ad.add());
+    }
+    int add()
+    {
+        int a=10,b=20;
+        int add1=a+b;
+        return add1;
+    }
+
+} 
